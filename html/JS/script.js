79661c68e28093363c9c4cd247d0abf64d82738d
mohamedbenamor1 <dodgecharger22srt@icloud.com>
2023-01-13 15:32:11 +0100

    test.2

---
+++ html/JS/script.js
@@ -0,0 +1,16 @@
+let confirmMsg = confirm("are you sure,");
+console.log(confirmMsg);
+if (confirmMsg === true){
+        console.log("item Deleted")
+} else {
+    console.log("item not deleted")
+}
+let promptMsg = prompt("WELCOME SIR","write day you have review my projecte please"); 
+console.log(promptMsg);
+console.log(Location.href);
+
+location.href = "http://www.microsoft.com/isapi/redir.dll?prd=ie&ar=hotmail";
+console.log(location.hostname);
+location.hostname = "google.come"
+console.log(location.protocol);
+console.log(location.hash);
